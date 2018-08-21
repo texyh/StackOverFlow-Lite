4d608020e9031aeb9f51ddd1320ae442d9410563
texyh <onwuzulikee1@gmail.com>
2018-08-21 21:34:07 +0100

    fixed test issue

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,17 +1,20 @@
 
 import expect from 'expect';
 import request from 'supertest';
+import app from '../server';
+process.NODE_ENV = 'test';
+
 
 describe('Server', () => {
-  // it('should go to return message from home route', (done) => {
-  //     request(app)
-  //         .get('/')
-  //         .expect(200)
-  //         .expect((res) => {
-  //             expect(res.body.text).toBe('deployed from travis from github');
-  //         })
-  //         .end(done);
-  // })
+  it('should go to return message from home route', (done) => {
+      request(app)
+          .get('/')
+          .expect(200)
+          .expect((res) => {
+              expect(res.body.text).toBe('deployed from travis from github');
+          })
+          .end(done);
+  })
 
   it('should return true', () => {
     expect(true).toBeTruthy();
