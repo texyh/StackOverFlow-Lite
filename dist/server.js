f0034234d991b4ac7dbd02f35b116bb7723987e1
Onwuzulike Emeka <onwuzulikee1@gmail.com>
2020-12-19 17:13:20 +0100

    minor changes

--- dist/server.js
+++ dist/server.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const db_1 = require("./db");
 const app_1 = require("./app");
+console.log(process.env.NODE_ENV);
 db_1.default().then(x => {
     app_1.default.listen(process.env.PORT, () => {
         console.log(`app started at port ${process.env.PORT}`);
--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@
 import dbConnection from './db';
 import app from './app';
 
+console.log(process.env.NODE_ENV)
 dbConnection().then(x => {
   app.listen(process.env.PORT, () => {
     console.log(`app started at port ${process.env.PORT}`);
