e52b3d15f47b8a38dbae402c764fa39b6e322540
Onwuzulike Emeka <onwuzulikee1@gmail.com>
2020-12-28 11:07:16 +0100

    added morgen and cors

--- app.ts
+++ app.ts
@@ -3,11 +3,17 @@ import * as bodyParser from 'body-parser';
 import questionRoutes from './src/questions/routes';
 import userRoutes from './src/users/routes';
 import answerRoutes from './src/answers/routes';
+import * as logger from 'morgan';
+import * as cors from 'cors';
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(logger('dev'));
+app.use(cors());
+
 app.get('/', (req, res) => res.send({ api: 'api version 1' }));
 app.use('/questions', questionRoutes);
 app.use('/users', userRoutes);
